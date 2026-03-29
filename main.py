ab5741b7a4e99230ea6ca6b2ef4fa7c8874b954a
meru55 <disney.movies.haha@gmail.com>
2026-03-30 02:02:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html")
